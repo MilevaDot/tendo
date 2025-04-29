879b00aae7d2376eb3bafc3ad7aced54adab921a
Harukamu <olimfactum@gmail.com>
2025-04-29 09:28:57 -0500

    last update

--- src/shared/components/collage/ImageGrid.tsx
+++ src/shared/components/collage/ImageGrid.tsx
@@ -4,19 +4,29 @@ import Masonry from 'react-masonry-css'
 import './ImageStyle.css'
 
 const ImageGrid = ({ imageList }:ImageGridProps) => {
+
+    const column1 = [imageList[0]]
+    const column2 = [imageList[1], imageList[2]]
+    const column3 = [imageList[3]]
+
+    const customColumns = [column1, column2, column3]
+
     const breakpointColumnsObj = {
-        default: imageList.length,
-        1100: imageList.length,
-        700: imageList.length,
+        default: 3,
+        1100: 3,
+        700: 1,
     }
+
     return (
         <Masonry
             breakpointCols={breakpointColumnsObj}
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column"
         >
-            {imageList.map((img, index) => (
-                <Box key={index} mb={4} overflow="hidden" borderRadius="10px">
+            {customColumns.map((column, index) => (
+                <Box key={index} >
+                    {column.map((img, idx) => (
+                    <Box key={idx} mb={4} overflow="hidden" borderRadius="10px">
                     <Image
                         src={img.src}
                         alt={img.alt}
@@ -27,9 +37,11 @@ const ImageGrid = ({ imageList }:ImageGridProps) => {
                         _hover={{
                             transform: 'scale(1.05)',
                             transition: 'transform 0.3s ease',
-                            boxShadow: 'lg',
+                            boxShadow: 'lg'
                         }}
                     />
+                    </Box>
+                    ))}
                 </Box>
             ))}
         </Masonry>
