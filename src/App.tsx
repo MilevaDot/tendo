0f7463e37ed418a13474ced8487408d99c5f7b52
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2025-05-02 12:36:45 -0500

    [FEAT]: Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import AppRoutes from "./router/AppRoutes"
 import './app.css'
 import { Toaster } from "sonner"
 import { UserProvider } from "./shared/context/UserContext"
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
         <ChakraProvider>
+          <Analytics />
           <Toaster richColors />
             <UserProvider>
               <AppRoutes />
